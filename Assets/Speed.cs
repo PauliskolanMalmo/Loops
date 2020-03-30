f916f1e6f5c8c72d88da0ee345a7d28d9454dfd6
Paul B <paul.belfrage@gmail.com>
2020-03-30 15:32:08 +0200

    Challenge Review How Fast You Going
    Challenge Review How Fast You Going

---
+++ Assets/Speed.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Speed : MonoBehaviour
+{
+    public int speed;
+    public int maxSpeed;
+
+    void Start()
+    {
+        maxSpeed = Random.Range(60, 120);
+        StartCoroutine(speedRoutine());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator speedRoutine()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(0.5f);
+            speed += 5;
+
+            if (speed > maxSpeed)
+            {
+                break;
+            }
+
+        }
+    }
+}
